80118219927e6f0d7aff929faec64f6994406512
Gabin Mathieu <gabin.mathieu4@etu.univ-lorraine.fr>
2024-05-14 10:17:03 +0200

    ajout dossiers

---
+++ test/TestChargeur.java
@@ -0,0 +1,34 @@
+package test;
+
+import XML.ChargeurMagasin;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.FileNotFoundException;
+
+class TestChargeur {
+
+
+    @Test
+    public void test_01_chargervalide() throws FileNotFoundException{
+        ChargeurMagasin c = new ChargeurMagasin("./musicbrainzSimple/");
+        c.chargerMagasin();
+    }
+
+    @Test
+    public  void test_02_chargerinvalide() throws Exception{
+        boolean raised = true;
+        try{
+            ChargeurMagasin c = new ChargeurMagasin("../musiqueCerveauSimple");
+            c.chargerMagasin();
+        } catch (Exception e){
+            System.out.println("erreur déclenchée avec succès");
+            raised = false;
+        }
+        if(raised){
+            System.out.println("hoyya");
+            throw new Exception("erreur : pas d'erreur");
+        }
+    }
+
+}
